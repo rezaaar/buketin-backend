ad987b2864d48c5e6f149f86414961d0a314cf34
rezaaar <rabbs3245@gmail.com>
2023-11-30 03:12:15 +0700

    settings

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(process.env.PORT || 3030);
 }
 bootstrap();
